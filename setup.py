e4ca8b4d566cc427486b6f7049191b9f90110e67
Emirhan Yılmaz Güney <56753978+yilmaz0734@users.noreply.github.com>
2021-02-22 22:25:47 +0300

    setup.py
    Setup file.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from cx_Freeze import setup,Executable
+import sys
+import PyQt5
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5 import QtWidgets
+from PyQt5 import uic
+from PyQt5 import QtCore
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+includefiles = ['667775.jpg','uygulamam.ui','notfound.png','Golden-Spotify-icon-png.ico']
+includes = []
+excludes = ['Tkinter']
+packages = ['os','PyQt5','spotipy','selenium','PyQt5.QtGui','PyQt5.QtWidgets','PyQt5.uic','PyQt5.QtCore','sys']
+
+setup(
+    name = 'CoverImageDownloader',
+    version = '0.1',
+    description = 'A general enhancement utility',
+    author = 'Yılmaz Güney',
+    author_email = 'emirhanyilmazguney@hotmail.com',
+    options = {'build_exe': {'includes':includes,'excludes':excludes,'packages':packages,'include_files':includefiles}}, 
+    executables = [Executable(script='main.py',base=base,icon='Golden-Spotify-icon-png.ico')]
+)
